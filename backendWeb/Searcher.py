cf5de0f6e818a73ac7c13b0c50f9e1041fae4c8a
Aidan Jiang <markedaidan1010@gmail.com>
2024-11-09 15:36:44 -0500

    Searcher plus my scrapestore(does not use files)

--- backendWeb/Searcher.py
+++ backendWeb/Searcher.py
@@ -1,19 +1,25 @@
-import json
-#Ask user for input
+
+from Aidanscrapestore import menu_data
+
 foodName = input("What do you desire: ")
 
-#gets the json file with dining hall and food
-with open('2024-11-09-Worcester.json', 'r') as file:
-    data = json.load(file)
+# Flag to check if food is found
+found_food = False
 
-#gets the date and location
-date = data["date"]
-location = data["location"]
-
-#parses through the file
-for item in data["meals"]:
-    if item["food"].lower() == foodName.lower():
-         print(f"Durimg {item['meal']} there is {foodName} in {location} on {date}.")
-           
-
+# Loop through all the entries in menu_data
+for key, data in menu_data.items():
+    date = data["date"]
+    location = data["location"]
+    
+    # Loop through meals for each day-location
+    for item in data["meals"]:
+        if item["food"].lower() == foodName.lower():
+            print(f"During {item['meal']} there is {foodName} in {location} on {date}.")
+            found_food = True  # Set the flag to True when food is found
+            break
+    
+if not found_food:
+    print("Sorry, it is not available.")
+        
+        
 
