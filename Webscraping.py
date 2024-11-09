440ac3c4cd8b9788e6e9701d741aa0aa039dcd70
Maya Nedkova <mnedkova@umass.edu>
2024-11-08 22:03:29 -0500

    Update Webscraping.py

--- Webscraping.py
+++ Webscraping.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
+from datetime import date
 import requests
-import csv
+ 
+"""Pages we want to scrape:
+https://umassdining.com/foodpro-menu-ajax?tid=1&date=11/09/2024
+"""
+
+
+day = date.today()
+
+month = date.month()
+
+year = date.year()
+
 
 page_to_scrape = requests.get("https://umassdining.com/locations-menus/worcester/menu")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
