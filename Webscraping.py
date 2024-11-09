6418e2ea5bb67b35ce28d4610e736f169c25cdf6
Aidan Jiang <markedaidan1010@gmail.com>
2024-11-08 21:37:50 -0500

    Create Webscraping.py

---
+++ Webscraping.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+page_to_scrape = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+quotes = soup.findAll("")
+
+
